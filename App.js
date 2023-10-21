2a3d71c69c639ac33f9009f9e3b370484f222915
Thanseehacp1 <taanzi0001@gmail.com>
2023-10-21 20:37:30 +0530

    project init

--- App.js
+++ App.js
@@ -1,20 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View ,Image} from 'react-native'
+import React ,{useState}from 'react'
 
-export default function App() {
+const App = () => {
+  const [enable, setEnable] = useState(false)
+  const  onClickMe=()=>{
+    console.log(enable);
+    setEnable(!enable);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex:1,
+      backgroundColor:'white',
+    }}>
+    
+    <View style={{
+     flex:1,
+      backgroundColor:enable?'#ffff8a':'black',
+    }}>
+      <View style={{
+      
+      backgroundColor:'white',
+      height:250,
+      width:5,
+      alignSelf:'center',
+    }}>
+     <View style={{
+      backgroundColor:'white',
+      height:400,
+      width:5,
+      marginLeft:100,
+      marginBottom:5,
+    }}>
     </View>
-  );
+      <TouchableOpacity 
+      onPress={
+        ()=>{
+          onClickMe();
+          }}
+          style={{
+        backgroundColor:enable?'white':'black',
+        height:50,
+        width:50,
+        borderRadius:50/2,
+       marginLeft:77,
+       marginTop:-20,
+       borderWidth:2,
+       borderColor:'grey',
+       alignContent:'center',
+       justifyContent:'center',
+       
+      }}>
+      <Image style={{
+        height:40,
+        width:40,
+        alignSelf:'center',
+        alignContent:'center',
+        justifyContent:'center',
+       }}
+      source={require("./assets/power3.png")}/>
+</TouchableOpacity>
+         </View>
+  
+ 
+  <Image style={{
+          height:150,
+          width:100,
+          marginTop:-55,
+          alignSelf:'center',
+        }}
+        resizeMode="contain"
+        source={enable? require("./assets/on2.png"):require("./assets/off2.png")}/>
+   
+   <View style={{
+    flex:2,
+}}>
+ <Text style={{
+    fontSize:20,
+    fontWeight:"bold",
+  color:enable?'black':'white',
+  marginLeft:40,
+  alignSelf:'auto',
+  marginTop:70,
+}}>Bedroom{'\n'}28°c</Text>
+  
+   </View>
+   </View>
+</View>
+
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+const styles = StyleSheet.create({})
